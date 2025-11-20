408bedbe2db327e67950388961c78e830c76edb8
Liza5557889 <elizavetalapina04@gmail.com>
2025-11-20 17:30:41 +0500

    Открывается и закрывается

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,4 +1,4 @@
-
+import { showBigPicture } from './big-picture.js';
 
 const createThumbnailElement = (photoData) => {
   const template = document.querySelector('#picture');
@@ -9,6 +9,12 @@ const createThumbnailElement = (photoData) => {
   thumbnail.querySelector('.picture__likes').textContent = photoData.likes;
   thumbnail.querySelector('.picture__comments').textContent = photoData.comments.length;
   thumbnail.dataset.thumbnailId = photoData.id;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showBigPicture(photoData);
+  });
+
   return thumbnail;
 };
 
@@ -23,3 +29,36 @@ const renderThumbnails = (photos) => {
 };
 
 export {renderThumbnails};
+
+/*
+
+const pictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderPhoto = (picture) => {
+  const {url, comments, likes} = picture;
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+
+  pictureElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    showBigPicture(picture);
+  });
+
+  return pictureElement;
+};
+
+const fragment = document.createDocumentFragment();
+
+const renderPhotos = (objects) => (
+  objects.forEach((item) => {
+    fragment.appendChild(renderPhoto(item));
+  });
+
+  pictures.appendChild(fragment);
+);
+*/
